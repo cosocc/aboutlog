95dcb777889c4769cca9f631c3a3684f15565027
光湖 杨 <121907007@qq.com>
2022-02-03 21:03:21 +0800

    添加项目文件。

---
+++ windowslogcode/main.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <string.h>
+#include <stdlib.h>
+#include "global.h"
+
+int main() 
+{
+    LogFile gLog("My.Log");
+    gLog.Log("test", 4);//记录日志
+    int i = 10;
+    while (i)
+    {
+        gLog.Log("你好啊!\r\n");
+        i--;
+    }
+    gLog.Log("系统启动");
+    //LogFileEx gLog(".", LogFileEx::YEAR);//一年生成一个日志文件
+    //LogFileEx gLog(".\\Log", LogFileEx::MONTH);//一月生成一个日志文件
+    LogFileEx gLog1(".\\Log", LogFileEx::DAY);//一天生成一个日志文件
+
+}
